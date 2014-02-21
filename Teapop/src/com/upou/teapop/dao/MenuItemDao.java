42365bcb9440b48004864c22be6838764677b1f3
maryjoy.pascual@upou.edu.ph <maryjoy.pascual@upou.edu.ph@22c77f39-4992-f29a-06e2-709b2e564467>
2014-02-21 17:53:38 +0000

    MenuDao

--- Teapop/src/com/upou/teapop/dao/MenuItemDao.java
+++ Teapop/src/com/upou/teapop/dao/MenuItemDao.java
@@ -1,29 +1,39 @@
 package com.upou.teapop.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import com.upou.teapop.data.MenuItem;
-
+import com.upou.teapop.data.Price;
 
 public class MenuItemDao extends BaseDao {
-
-	public boolean createItem(MenuItem MenuItem) {
+	
+	private static final String MENU_CREATE   = "INSERT INTO teapop.menu (menu_id, menu_code, category_id, name, description, currency, regular_price, small_price, large_price, position, featured, hidden) on duplicate key update" +
+                                               "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	
+	private static final String MENU_VIEW     = "SELECT * FROM teapop.menu WHERE menu_id = ?";
+	
+	private static final String MENU_DELETE	  = "DELETE FROM person WHERE id = ?";
+	
+	public boolean createItem(MenuItem menuItem) {
 		boolean result = false;
 		try {
 			Connection conn = createConnection();
-			Statement stmt = conn.createStatement();
-
-			String query = "INSERT INTO menu (menu_id, price) "
-					+ "VALUES ('"
-					+ MenuItem.getItemCode()
-					+ "')";
-
-			stmt.executeUpdate(query);
+			PreparedStatement stmt = conn.prepareStatement(MENU_CREATE);
+			stmt.setInt(1, menuItem.getItemId());
+			stmt.setString(2, menuItem.getItemCode());
+			stmt.setInt(3, menuItem.getCatId());
+			stmt.setString(4, menuItem.getName());
+			stmt.setString(5, menuItem.getDesc());
+			stmt.setString(6, menuItem.getPrice().getCurrency());
+			stmt.setDouble(7, menuItem.getPrice().getRegular());
+			stmt.setDouble(8, menuItem.getPrice().getSmall());
+			stmt.setDouble(9, menuItem.getPrice().getLarge());
+			stmt.setInt(10, menuItem.getDispPosition());
+			stmt.setString(11, menuItem.getFeatured());
+			stmt.setString(12, menuItem.getHidden());
+			stmt.execute();
 			result = true;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -33,21 +43,28 @@ public class MenuItemDao extends BaseDao {
 
 	
 	
-
-	public MenuItem retrieveItem(int itemId) {
-		MenuItem item = new MenuItem();
+	public MenuItem retrieveItem(int menuId) {
+		MenuItem menuItem = new MenuItem();
 		try {
 			Connection conn = createConnection();
 
-			// get category info
-			Statement stmt = conn.createStatement();
-			String query = "SELECT * FROM teapop.product";
-			ResultSet rs = stmt.executeQuery(query);
+			PreparedStatement stmt = conn.prepareStatement(MENU_VIEW);
+			ResultSet rs = stmt.executeQuery();
 
 			while (rs.next()) {
-				item.setDesc(rs.getString("description"));
-				item.setItemId(rs.getInt("product_id"));
-				item.setName(rs.getString("name"));
+				menuItem.setItemCode(rs.getString("menu_code"));
+				menuItem.setCatId(rs.getInt("category_id"));
+				menuItem.setName(rs.getString("name"));
+				menuItem.setDesc(rs.getString("description"));
+				Price price = new Price();
+				price.setCurrency(rs.getString("currency"));
+				price.setRegular(rs.getDouble("price"));
+				price.setSmall(rs.getDouble("price_small"));
+				price.setLarge(rs.getDouble("price_big"));
+				menuItem.setPrice(price);
+				menuItem.setDispPosition(rs.getInt("position"));
+				menuItem.setFeatured(rs.getString("featured"));
+				menuItem.setHidden(rs.getString("hidden"));
 			}
 			conn.close();
 			stmt.close();
@@ -56,34 +73,18 @@ public class MenuItemDao extends BaseDao {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return item;
+		return menuItem;
 	}
 
-	public boolean updateItem(MenuItem menuItem) {
-		boolean result = false;
-		try {
-			Connection conn = createConnection();
-			Statement stmt = conn.createStatement();
-
-			String query = "INSERT VALUES() INTO menu";
-
-			stmt.executeQuery(query);
-			result = true;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
 	
-	public boolean deleteItem(String id) {
+	public boolean deleteItem(int id) {
 		boolean result = false;
 		try {
 			Connection conn = createConnection();
-			Statement stmt = conn.createStatement();
-
-			String query = "DELETE FROM items WHERE id=" + id;
-
-			stmt.executeQuery(query);
+			PreparedStatement stmt = conn.prepareStatement(MENU_DELETE);
+			stmt.setInt(1, id);
+			stmt.executeUpdate();
+ 
 			result = true;
 		} catch (Exception e) {
 			e.printStackTrace();
