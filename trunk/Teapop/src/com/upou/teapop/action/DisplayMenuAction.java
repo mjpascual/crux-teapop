b508d40e99979185f1db34d6b1d2919bebabb77f
timjosef.tan@upou.edu.ph <timjosef.tan@upou.edu.ph@22c77f39-4992-f29a-06e2-709b2e564467>
2014-02-22 11:21:10 +0000

    removed unused imports

--- trunk/Teapop/src/com/upou/teapop/action/DisplayMenuAction.java
+++ trunk/Teapop/src/com/upou/teapop/action/DisplayMenuAction.java
@@ -2,11 +2,7 @@ package com.upou.teapop.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.upou.teapop.dao.MenuItemDao;
-import com.upou.teapop.data.Category;
 import com.upou.teapop.data.Menu;
-import com.upou.teapop.data.MenuItem;
-import com.upou.teapop.data.MenuSection;
-import com.upou.teapop.data.Price;
 
 
 public class DisplayMenuAction extends ActionSupport{
